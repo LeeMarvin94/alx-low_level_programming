6e546dc8c080d631cc8fff7165db9b475ef73d57
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-08 10:15:46 -0700

    My fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  *
  *  * main - Entry point
@@ -8,9 +9,14 @@
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
+
