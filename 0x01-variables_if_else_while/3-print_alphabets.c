e16c04bd0fc88be65d127dd7f7a57ebf2cfc9664
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-10 03:12:33 +0100

    My fourth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   char alp1[] = "abcdefghijklmnopqrstuwxyz";
+   char alp2[] = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+   int i;
+
+   for (i = 0; i < 26; i++)
+   {       
+             putchar(alp1[i]);
+   }
+   for (i = 0; i < 26; i++)
+   {
+             putchar(alp2[i]);
+   }
+   putchar('\n');
+   return (0);
+
+ 
+}
