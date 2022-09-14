0036311cfd35db45b544bb878b34f69d65fba967
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-15 00:54:31 +0100

    fourth commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks the lower
+ *  character to check the case
+ * between 0 and 1
+ */
+
+int _islower(int c)
+
+{
+
+	return (c >= 97 && c <= 122);
+
+
+
+}
