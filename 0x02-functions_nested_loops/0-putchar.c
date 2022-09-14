b2767feddb5fac0e5a147a6675565c6cc7d0d363
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-14 16:08:24 -0700

    My first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,31 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main - Prints _putchar
- *
- * Return: Always 0 (Success)
+ * main - print puchar
+ * Return: always 0
  */
-int main(void)
+
+ int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	return (0);
+         _putchar('H');
+
+        _putchar('o');
+
+        _putchar('l');
+
+        _putchar('b');
+
+        _putchar('e');
+
+        _putchar('r');
+
+        _putchar('t');
+
+        _putchar('o');
+
+        _putchar('n');
+
+        _putchar('\n');
+
+        return (0);
 }
