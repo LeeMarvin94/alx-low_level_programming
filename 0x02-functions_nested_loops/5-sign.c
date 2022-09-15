53d2dbc61c21279c38495ea0953985caf530c489
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-15 01:06:37 +0100

    sixth commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,41 +1,48 @@
-julien@ubuntu:~/0x02$ cat 5-main.c
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
  */
-int main(void)
-{
-    int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int print_sign(int n)
+
+{
+
+	int numz;
+
+
+
+	if (n > 0)
+
+	{
+
+		numz = 1;
+
+		_putchar('+');
+
+	}
+
+	else if (n == 0)
+
+	{
+
+		numz = 0;
+
+		_putchar('0');
+
+	}
+
+	else
+
+	{
+
+		numz = -1;
+
+		_putchar('-');
+
+	}
+
+	return (numz);
+
 }
-julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-sign.c -o 5-sign
-julien@ubuntu:~/0x02$ ./5-sign 
-+, 1
-0, 0
-+, 1
--, /
-julien@ubuntu:~/0x02$ 
