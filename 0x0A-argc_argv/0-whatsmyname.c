f09235dbf1dbeb84ae3264263193683e7ae86c77
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2022-09-30 12:21:35 +0100

    First commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,12 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * _puts_recursion - prints a string followed by a new line
- * @s: character to be printed
- * Return: returns void
- */
-
-void _puts_recursion(char *s)
+* main -  writes program name
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char *argv[])
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
-	}
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
