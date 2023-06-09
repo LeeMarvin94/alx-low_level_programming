6a294404f90419d9191c080833bc8bfc3baf92dc
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-06-09 12:19:43 +0100

    Edit the task 2

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Add positive numbers + } =
+ *
+ * @argc: number of parameters given to the program
+ * @argc: array of pointers to the string of the given parameters
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+/* If no number is passed as argument */
+if (argc == 1)
+{
+printf("%d\n",0);
+}
+
+for(i = 1; i < argc; i++)
+{
+if (*(argv + i)
+}
+
+}
