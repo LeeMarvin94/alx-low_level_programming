e823952774c31d827d07f6846036ef24b1c4824f
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-06-09 12:45:48 +0100

    Add the task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,23 +4,38 @@
  * main - Add positive numbers + } =
  *
  * @argc: number of parameters given to the program
- * @argc: array of pointers to the string of the given parameters
+ * @argv: array of pointers to the string of the given parameters
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-int i;
+int i, sum = 0;
 
 /* If no number is passed as argument */
 if (argc == 1)
 {
-printf("%d\n",0);
+printf("%d\n", 0);
+return (1);
 }
 
-for(i = 1; i < argc; i++)
+/* Verifies all the given parameters are digits if throw an error */
+for (i = 1; i < argc; i++)
 {
-if (*(argv + i)
+if (**(argv + i) < 48 || **(argv + i) > 57)
+{
+printf("Error\n");
+return (1);
 }
 
+if (atoi(argv[i]) > 0)
+{
+sum += atoi(argv[i]);
+}
+
+}
+/* Displaying of the result */
+printf("%d\n", sum);
+
+return (0);
 }
