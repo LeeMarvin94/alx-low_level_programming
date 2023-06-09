bbf670e308d9c31b250892e9d5ff5f19827af97c
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-06-09 10:40:21 +0100

    To add the task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it + } =
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: representing the array containing the arguments passed to the program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+printf("%d\n",argc);
+
+return (0);
+}
