8509fdc398d8d7d768fabb73b0c8a863f1433fda
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-06-09 11:34:18 +0100

    Add the task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints all arguments it receives + } =
+ *
+ * @argc: count the number of parameters givento the program
+ * @argv: contains pointers of the parameters
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n",*(argv + i));
+}
+return (0);
+}
