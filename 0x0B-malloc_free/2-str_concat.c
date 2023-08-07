ce5ef07ddf80a47548b7e879c316b9b53a00db85
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-08-08 00:19:20 +0100

    Edit task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,32 +17,34 @@ int counts1 = 0;
 int counts2 = 0;
 char *ptr_s;
 
+if (s1 == NULL || s2 == NULL)
+{
+if (s1 == NULL)
+*s1 = '\0';
+else
+*s2 = '\0';
+}
 while (s1[i] != '\0')
 {
 counts1++;
 i++;
 }
-
 while (s2[j] != '\0')
 {
 counts2++;
 j++;
 }
-
 ptr_s = malloc(sizeof(char) * (counts1 + counts2 + 1));
 if (ptr_s == NULL)
 return (NULL);
-
 for (i = 0; i < counts1; i++)
 ptr_s[i] = s1[i];
-
 j = 0;
 for (i = counts1; i < (counts2 + counts1); i++)
 {
 ptr_s[i] = s2[j];
 j++;
 }
-
 ptr_s[i] = '\0';
 return (ptr_s);
 }
