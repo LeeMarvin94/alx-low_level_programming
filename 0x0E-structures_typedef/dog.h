462a442bd8c047d7e7d84c2746d63e6ec4d64494
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2023-08-14 13:29:14 +0100

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Dog structure
+ * @name: represents the name of the Dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog{
+
+char *name;
+float age;
+char *owner;
+
+};
+
+
+#endif
